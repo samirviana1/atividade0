5cbe5078e4f0c1c68b107f9a086d20893c0df91d
samirviana1 <samir_viana1@hotmail.com>
2022-07-22 09:15:21 -0300

    atividadeCondicional

---
+++ public/ativiJs/ativiCondi.js
@@ -0,0 +1,76 @@
+/*let idade = Number(prompt("Digite a idade do motorista: "))
+   if (idade>= 18) {
+    console.log("Pode dirigir", idade)
+   }else{
+    console.log("Não pode dirigir.", idade)
+   }
+*/
+//-----------------------------------------------------------------
+
+/*let idade = Number(prompt("Digite a idade do motorista: "))
+let carteiraMotorista = (prompt("Digite se o motorista é Habilitado (S) para sim e (N) para não: ")).toUpperCase()
+   if (idade>= 18 && carteiraMotorista === 'S') {
+    console.log("Pode dirigir", idade)
+   } else if (idade < 18 || carteiraMotorista === 'N'){
+    console.log("Não pode dirigir.", idade)
+   }
+   */
+//----------------------------------------------------------------------------
+/*let dia = Number(prompt("Digite de 1 a 7 para saber o dia da semana: "))
+
+   if (dia === 1) {
+      document.write(dia, ' Domingo')
+   } else if (dia === 2){
+      document.write(dia, ' Segunda')
+   } else if(dia === 3){
+      document.write(dia, ' Terça')
+   }else if(dia === 4){
+      document.write(dia, ' Quarta')
+   }else if(dia === 5){
+      document.write(dia, ' Quinta')
+   }else if(dia ===6){
+      document.write(dia, ' Sexta')
+   }else if(dia === 7){
+      document.write(dia, ' Sabado')
+   }else{
+    document.write('Algo deu errado!, Digite um numero de 1 a 7')
+   }
+*/
+//------------------------------------------------------------------------------
+/*
+let dia = Number(prompt("Digite de 1 a 7 para saber o dia da semana: "));
+
+switch (dia) {
+  case 1:
+    document.write(dia, " Domingo");
+    break;
+
+  case 2:
+    document.write(dia, " Segunda");
+    break;
+
+  case 3:
+    document.write(dia, " Terça");
+    break;
+
+  case 4:
+    document.write(dia, " Quarta");
+    break;
+
+  case 5:
+    document.write(dia, " Quinta");
+    break;
+
+  case 6:
+    document.write(dia, " Sexta");
+    break;
+
+  case 7:
+    document.write(dia, " Sabado");
+    break;
+
+  default:
+    document.write("Algo deu errado!, Digite um numero de 1 a 7");
+    break;
+}
+*/
